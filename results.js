aee8a1ea8ced0868fec5471be6203c1382bf195c
Shirley Lu <xlu2@wellesley.edu>
2013-09-27 10:20:23 -0400

    made buttons white

--- results.js
+++ results.js
@@ -8,6 +8,12 @@ var rLoad = function () {
 var addLine = function (o) {
 	var r = document.createElement("div");
 	r.setAttribute("class","resultline");
-	r.innerHTML = Math.floor(o.time/60)+":"+o.time%60+","+o.team+","+o.type;
+	r.innerHTML = Math.floor(o.time/60)+":";
+	var s = o.time%60;
+	if (s<10)
+		r.innerHTML += "0"+s+","+o.team+","+o.type;
+	else
+		r.innerHTML += s+","+o.team+","+o.type;
+	console.log(r.innerHTML);
 	gamer.appendChild(r);
 }
